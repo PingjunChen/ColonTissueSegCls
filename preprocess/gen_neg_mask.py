f0da96532dc8e1e5716b5d9b7bde6b9ef972668f
PingjunChen <chenpingjun@gmx.com>
2019-07-25 13:20:23 -0400

    rewrite neg mask generation

--- preprocess/gen_neg_mask.py
+++ preprocess/gen_neg_mask.py
@@ -10,23 +10,22 @@ PIL.Image.MAX_IMAGE_PIXELS = 933120000
 import pydaily
 
 
-def gen_neg_mask(ori_neg_dir, save_neg_dir):
-    img_list = pydaily.filesystem.find_ext_files(ori_neg_dir, "jpg")
+def gen_neg_mask(neg_dir):
+    img_list = pydaily.filesystem.find_ext_files(neg_dir, "jpg")
 
     for ind, ele in enumerate(img_list):
         ele = os.path.basename(ele)
         if ind > 0 and ind % 10 == 0:
             print("Processing {}/{}".format(ind, len(img_list)))
-        cur_img_path = os.path.join(ori_neg_dir, ele)
+        cur_img_path = os.path.join(neg_dir, ele)
         save_name = str(uuid.uuid4())[:8]
-        shutil.copy(cur_img_path, os.path.join(save_neg_dir, save_name+".jpg"))
         cur_img = io.imread(cur_img_path)
         mask_arr = np.zeros(cur_img.shape[:2], dtype=np.uint8)
-        save_mask_path = os.path.join(save_neg_dir, save_name+".png")
-        io.imsave(save_mask_path, mask_arr)
+        os.rename(cur_img_path, os.path.join(neg_dir, save_name+".jpg"))
+        mask_path = os.path.join(neg_dir, save_name+".png")
+        io.imsave(mask_path, mask_arr)
 
 if __name__ == "__main__":
     np.random.seed(1234)
-    ori_neg_dir = "../data/tissue-train-neg"
-    save_neg_dir = "../data/NegImgs"
-    gen_neg_mask(ori_neg_dir, save_neg_dir)
+    neg_dir = "../data/tissue-train-neg"
+    gen_neg_mask(neg_dir)
