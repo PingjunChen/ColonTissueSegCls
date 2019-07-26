df1c60a18d4783f59dfc7c5ec39a7a2be3b7cc46
PingjunChen <chenpingjun@gmx.com>
2019-07-26 15:35:26 -0400

    add patch classification model

---
+++ patch-cls/test_cls.py
@@ -0,0 +1,38 @@
+
+# -*- coding: utf-8 -*-
+
+import os, sys
+import argparse
+import torch
+import torch.backends.cudnn as cudnn
+
+from train_eng import validate_model
+from patchloader import val_loader
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description='Thyroid Classification')
+    parser.add_argument('--seed',            type=int,   default=1234)
+    parser.add_argument('--batch_size',      type=int,   default=64)
+    parser.add_argument('--model_dir',       type=str,   default="../data/PatchCLS/Models")
+    parser.add_argument('--model_name',      type=str,   default="resnet50")
+    parser.add_argument('--model_path',      type=str,   default="")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    args = set_args()
+    torch.cuda.manual_seed(args.seed)
+    cudnn.benchmark = True
+
+    model_full_path = os.path.join(args.model_dir, args.model_name, args.model_path)
+    ft_model = torch.load(model_full_path)
+    ft_model.cuda()
+    test_data_loader = val_loader(args.batch_size)
+
+    print("Start testing...")
+    test_acc = validate_model(test_data_loader, ft_model)
+    print("Testing accuracy is: {:.3f}".format(test_acc))
