357f46b1a4bcbd2493348f023969c5a2416f2b4e
PingjunChen <chenpingjun@gmx.com>
2019-07-29 21:21:17 -0400

    update patch seg code

--- preprocess/gen_seg_patch.py
+++ preprocess/gen_seg_patch.py
@@ -8,18 +8,22 @@ import PIL
 PIL.Image.MAX_IMAGE_PIXELS = None
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
 
 import pydaily
 from pyslide import patch
 
 
-def gen_patches(img_dir, patch_dir, patch_size=512):
+def gen_patches(img_dir, patch_dir, patch_size=448):
     img_list = pydaily.filesystem.find_ext_files(img_dir, "jpg")
     img_list = [os.path.basename(ele) for ele in img_list]
     patch_img_dir = os.path.join(patch_dir, "imgs")
-    pydaily.filesystem.overwrite_dir(patch_img_dir)
+    if not os.path.exists(patch_img_dir):
+        os.makedirs(patch_img_dir)
     patch_mask_dir = os.path.join(patch_dir, "masks")
-    pydaily.filesystem.overwrite_dir(patch_mask_dir)
+    if not os.path.exists(patch_mask_dir):
+        os.makedirs(patch_mask_dir)
 
     pos_num, neg_num = 0, 0
     for ind, ele in enumerate(img_list):
@@ -40,16 +44,17 @@ def gen_patches(img_dir, patch_dir, patch_size=512):
             # mask control
             patch_mask = cur_mask[start_h:start_h+patch_size, start_w:start_w+patch_size]
             pixel_ratio = np.sum(patch_mask > 0) * 1.0 / patch_mask.size
-            save_flag = False
+            save_flag = True
             if pixel_ratio >= 0.10:
-                save_flag = True
                 pos_num += 1
             else:
-                if np.random.random_sample() > 0.95:
-                    save_flag = True
-                    neg_num += 1
+                if "neg" in img_dir:
+                    if np.random.random_sample() > 0.88:
+                        neg_num += 1
+                    else:
+                        save_flag = False
                 else:
-                    save_flag = False
+                    neg_num += 1
             if save_flag == True:
                 patch_name = str(uuid.uuid4())[:8]
                 io.imsave(os.path.join(patch_img_dir, patch_name+".jpg"), patch_img)
@@ -58,12 +63,6 @@ def gen_patches(img_dir, patch_dir, patch_size=512):
 
 
 if __name__ == "__main__":
-    # # generate patches by neg
-    # img_dir = "../data/tissue-train-neg/train"
-    # patch_dir = "../data/Patches/train"
-    # gen_patches(img_dir, patch_dir)
-
-    # generate patches by pos
     img_dir = "../data/tissue-train-pos/train"
-    patch_dir = "../data/Patches/train"
+    patch_dir = "../data/SegPatches/train"
     gen_patches(img_dir, patch_dir, patch_size=448)
