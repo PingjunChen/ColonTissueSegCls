2e819cd572c6906837de5c273e0a080da9b2fb7d
PingjunChen <chenpingjun@gmx.com>
2019-09-05 17:58:57 -0400

    add overlay selected suspicious regions

--- wsi-cls/pred_slide_cls.py
+++ wsi-cls/pred_slide_cls.py
@@ -18,6 +18,7 @@ from torch.utils.data import DataLoader
 from torch.autograd import Variable
 from patch_loader import PatchDataset, wsi_stride_splitting
 from wsinet import WsiNet
+from utils import overlayWSI
 
 
 def extract_model_feas(patch_model, input_tensor):
@@ -53,12 +54,13 @@ def gen_wsi_feas(patch_model, img_path, args):
         # image background control
         if patch.patch_bk_ratio(patch_img, bk_thresh=0.864) <= 0.88:
             patch_list.append(patch_img)
+            coor_list.append([start_h, start_w, start_h+args.patch_len, start_w+args.patch_len])
 
         # Processing the feature extraction in batch-wise manner to avoid huge memory consumption
-        if len(patch_list) == 64 or ind+1 == len(coors_arr):
+        if len(patch_list) == args.batch_size or ind+1 == len(coors_arr):
             patch_arr = np.asarray(patch_list)
             patch_dset = PatchDataset(patch_arr)
-            patch_loader = DataLoader(patch_dset, batch_size=64, shuffle=False, num_workers=4, drop_last=False)
+            patch_loader = DataLoader(patch_dset, batch_size=args.batch_size, shuffle=False, num_workers=4, drop_last=False)
             with torch.no_grad():
                 for inputs in patch_loader:
                     batch_tensor = Variable(inputs.cuda())
@@ -73,12 +75,12 @@ def gen_wsi_feas(patch_model, img_path, args):
     all_probs = np.asarray(probs_list).astype(np.float32)
     sorted_ind = np.argsort(all_probs[:, 0])
 
-    TEST_PATCH_NUM = 12
-    feas_placeholder = np.zeros((TEST_PATCH_NUM, all_feas.shape[1]), dtype=np.float32)
-    test_patch_num = min(len(all_feas), TEST_PATCH_NUM)
+    feas_placeholder = np.zeros((args.test_patch_num, all_feas.shape[1]), dtype=np.float32)
+    test_patch_num = min(len(all_feas), args.test_patch_num)
     chosen_total_ind = sorted_ind[:test_patch_num]
     feas_placeholder[:test_patch_num] = all_feas[chosen_total_ind]
-    return feas_placeholder, test_patch_num
+    chosen_coors = np.asarray(coor_list)[chosen_total_ind].tolist()
+    return feas_placeholder, test_patch_num, chosen_coors
 
 
 
@@ -86,13 +88,17 @@ def set_args():
     parser = argparse.ArgumentParser(description="Colon slides classification")
     parser.add_argument('--seed',          type=int,  default=1234)
     parser.add_argument('--device_id',     type=str,  default="4",  help='which device')
-    parser.add_argument('--img_dir',       type=str,  default="../data/SlideCLS/Split1235/SlideImgs/tissue-train-neg/val")
-    parser.add_argument('--patch_model',   type=str,  default="../data/PatchCLS/Split1235/Models/resnet50/04-0.897.pth")
-    parser.add_argument('--wsi_model',     type=str,  default="../data/SlideCLS/Split1235/WsiModels/resnet50/selfatt/epoch_030_acc_0.985_tn_080_fp_002_fn_000_tp_050.pth")
+    parser.add_argument('--img_dir',       type=str,  default="../data/SlideCLS/Split1234/SlideImgs/tissue-train-pos/val")
+    parser.add_argument('--patch_model',   type=str,  default="../data/PatchCLS/Split1234/Models/resnet50/05-0.833.pth")
+    parser.add_argument('--wsi_model_dir', type=str,  default="../data/SlideCLS/Split1234/WsiModels/resnet50")
     parser.add_argument('--fusion_mode',   type=str,  default="selfatt")
+    parser.add_argument('--wsi_model_name',type=str,  default="epoch_099_acc_0.977_tn_080_fp_002_fn_001_tp_049.pth")
     parser.add_argument('--class_num',     type=int,  default=2)
-    parser.add_argument('--stride_len',    type=int,  default=256)
+    parser.add_argument('--stride_len',    type=int,  default=224)
     parser.add_argument('--patch_len',     type=int,  default=448)
+    parser.add_argument('--batch_size',    type=int,  default=128)
+    parser.add_argument('--test_patch_num',type=int,  default=12)
+    parser.add_argument('--save_overlay',  action='store_true', default=False)
     args = parser.parse_args()
     return args
 
@@ -108,7 +114,8 @@ if __name__ == "__main__":
 
     # load wsi cls model
     wsinet = WsiNet(class_num=2, in_channels=2048, mode=args.fusion_mode)
-    wsi_weights_dict = torch.load(args.wsi_model, map_location=lambda storage, loc: storage)
+    wsi_weights_path = os.path.join(args.wsi_model_dir, args.fusion_mode, args.wsi_model_name)
+    wsi_weights_dict = torch.load(wsi_weights_path, map_location=lambda storage, loc: storage)
     wsinet.load_state_dict(wsi_weights_dict)
     wsinet.cuda()
     wsinet.eval()
@@ -120,12 +127,21 @@ if __name__ == "__main__":
     correct_num = 0
     for test_slide in test_slide_list:
         test_slide_path = os.path.join(args.img_dir, test_slide)
-        chosen_feas, chosen_num = gen_wsi_feas(patch_model, test_slide_path, args)
+        chosen_feas, chosen_num, chosen_coors = gen_wsi_feas(patch_model, test_slide_path, args)
         slide_fea_data = torch.from_numpy(chosen_feas).unsqueeze(0)
         im_data = Variable(slide_fea_data.cuda())
         true_num = torch.from_numpy(np.array([chosen_num]))
-        cls_probs, _ = wsinet(im_data, None, true_num=true_num)
+        cls_probs, assignments = wsinet(im_data, None, true_num=true_num)
         test_prob = cls_probs.cpu().data.numpy()[0][1]
         if test_prob >  0.5:
             correct_num += 1
+
+        if args.save_overlay:
+            weights = assignments[0].data.cpu().tolist()
+            overlay_wsi = overlayWSI(test_slide_path, chosen_coors, weights)
+            overlay_save_dir = os.path.join(os.path.dirname(args.img_dir), "overlay")
+            pydaily.filesystem.overwrite_dir(overlay_save_dir)
+            io.imsave(os.path.join(overlay_save_dir, test_slide), overlay_wsi)
+
+
     print("Testing accuracy is {}/{}".format(correct_num, total_num))
--- wsi-cls/test_slide_cls.py
+++ wsi-cls/test_slide_cls.py
@@ -13,10 +13,10 @@ from wsi_test_engine import load_wsinet, test_cls
 def set_args():
     # Arguments setting
     parser = argparse.ArgumentParser(description="TCT slides classification")
-    parser.add_argument('--test_data_dir',   type=str,          default="../data/SlideCLS/Split1235/SlideFeas/resnet50")
+    parser.add_argument('--feas_dir',        type=str,          default="../data/SlideCLS/Split1235/SlideFeas")
     parser.add_argument('--model_dir',       type=str,          default="../data/SlideCLS/Split1235/WsiModels")
     parser.add_argument('--batch_size',      type=int,          default=16,         help='batch size.')
-    parser.add_argument('--device_id',       type=str,          default="5",        help='which device')
+    parser.add_argument('--device_id',       type=str,          default="4",        help='which device')
     parser.add_argument('--cnn_model',       type=str,          default="resnet50", help='cnn model')
     parser.add_argument('--fusion_mode',     type=str,          default="selfatt")
     parser.add_argument('--wsi_cls_name',    type=str,          default="epoch_030_acc_0.985_tn_080_fp_002_fn_000_tp_050.pth")
@@ -34,7 +34,7 @@ if  __name__ == '__main__':
     # prepare model
     wsinet = load_wsinet(args)
     # prepare dataset
-    test_data_dir = os.path.join(args.test_data_dir, "val")
+    test_data_dir = os.path.join(args.feas_dir, args.cnn_model, "val")
     test_dataset = wsiDataSet(test_data_dir, pre_load=True, testing=True)
     test_dataloader = DataLoader(dataset=test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4, pin_memory=True)
     # start testing
